f4513e01ac75466d13f783b6ab0afa526a21155c
Jony <jony@AC900166.ipt.aol.com>
2017-04-20 16:14:40 +0800

    perfect the MBProgressHUD

--- LZHGRControl/LZSet/SetView/SetKeyboardView.m
+++ LZHGRControl/LZSet/SetView/SetKeyboardView.m
@@ -11,7 +11,7 @@
 //#define ROOMNUBER @”0123456789”
 
 @interface SetKeyboardView ()<UITextFieldDelegate>
-@property (nonatomic, strong) MBProgressHUD *HUD;
+//@property (nonatomic, strong) MBProgressHUD *HUD;
 
 @property (nonatomic, strong) UDPNetwork *sharedUDPNetwork;
 
@@ -38,14 +38,14 @@
         self.layer.cornerRadius = 18;
         self.layer.masksToBounds = YES;
         
-        //初始化ip显示
+        //初始化ip文本框显示
         self.ipTextField.text = [self.sharedUDPNetwork.networkInfoDic objectForKey:@"host"];
         self.ipTextField.clearButtonMode = UITextFieldViewModeAlways;
         self.ipTextField.clearsOnBeginEditing = YES;
         self.ipTextField.keyboardType = UIKeyboardTypeDecimalPad;
         self.ipTextField.delegate = self;
         
-        //房间号
+        //房间号文本框
         self.roomNumTextField.clearButtonMode = UITextFieldViewModeAlways;
         self.roomNumTextField.clearsOnBeginEditing = YES;
         self.roomNumTextField.keyboardType = UIKeyboardTypeDecimalPad; //数字键盘 有数字和小数点
@@ -54,6 +54,8 @@
       }
     return self;
 }
+
+//switch用于测试网络接收
 - (IBAction)swValueChanged:(id)sender{
     [DMCore sharedDMCore].PLAircon.temperature++;
     if ([(UISwitch *)sender isOn]) {
@@ -67,10 +69,12 @@
 }
 
 #pragma mark - 按钮事件
-
+//取消，使IP地址显示为原来的ip地址
 - (IBAction)cancel:(id)sender {
       self.ipTextField.text = [self.sharedUDPNetwork.networkInfoDic objectForKey:@"host"];
 }
+
+//保存ip输入框中的ip地址
 - (IBAction)preserve:(id)sender {
     //保存前检查IP地址规范
     if (![self isIPAddressWithString:self.ipTextField.text]) {
@@ -79,8 +83,10 @@
         return;
     }
     
+    //更新正在使用的ip地址
     [self.sharedUDPNetwork.networkInfoDic setValue:self.ipTextField.text forKey:@"host"];
     
+    //更新存储的ip地址
     if ([self.sharedUDPNetwork renewLocalNetworkInfo]) {
         NSLog(@"保存成功");
         [self showMessage:@"保存成功！"];
@@ -92,13 +98,15 @@
 }
 
 #pragma mark - 文本处理
+//根据四位房间号，构建ip地址
 - (NSString *)buildIPStringWithString:(NSString *)string{
    
-    //检查数据规范
+    //检查房间号规范
     if (![self isFourBitOfRoomNumbersWithString:string]) {
         return [self.sharedUDPNetwork.networkInfoDic objectForKey:@"host"];
     }
     
+    //分别截取房间号的前两位和后两位，作为ip地址的最后两格
     NSString *precedTwoSubStr = [string substringToIndex:2];
     NSString *backTwoSubStr = [string substringFromIndex:2];
     
@@ -112,6 +120,7 @@
         return false;
     }
     
+    //检查输入的房间号是否都是数字
     for (NSInteger i = 0; i < string.length; i++) {
         unichar charactor = [string characterAtIndex:i];
         if (!(charactor  >= 48 && charactor  <=57)) {
@@ -163,45 +172,72 @@
 
 //提示信息
 - (void)showMessage:(NSString *)string{
-    self.HUD = [[MBProgressHUD alloc] initWithView:self];
-    [self addSubview:self.HUD];
     
-    self.HUD.labelText = string;
-    self.HUD.mode = MBProgressHUDModeText;
+    //此方法，如果在用户快速连续操作下，可能导致页面上有多余的提示未被清除，
+    //解决方案1：采用后台,采用后台时与下方的延迟执行冲突,如改为全局线程后台执行，亦不能解决
+    //解决方案2：把原本的类属性HUB改为，此方法中的局部变量，当此方法执行完后，所有的都会由ARC自动释放，用户操作如何快速都不会导致有未被清除的提示
+
+    //self本身就是View
+    MBProgressHUD *HUD = [[MBProgressHUD alloc] initWithView:self];
+    [self addSubview:HUD];
+    
+    HUD.labelText = string;
+    HUD.mode = MBProgressHUDModeText;
     
     //指定距离中心点的X轴和Y轴的偏移量，如果不指定则在屏幕中间显示
-    //    HUD.yOffset = 150.0f;
-    //    HUD.xOffset = 100.0f;
+    HUD.yOffset = -100.0f;
+    HUD.xOffset = 0.0f;
     
-    [self.HUD showAnimated:YES];
+    //采用GCD延迟1S，直接取消提示
+    [HUD showAnimated:YES];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self.HUD removeFromSuperview];
-        self.HUD = nil;
+        [HUD removeFromSuperview];
+//        HUD = nil;
     });
-//    [self.HUD showAnimated:YES whileExecutingBlock:^{
-//        sleep(2);
-//    } completionBlock:^{
-//        [self.HUD removeFromSuperview];
-////        [HUD release];
-//        self.HUD = nil;
-//    }];
     
+//    //会产生警告，采用GCD，如上
+//    [HUD showAnimated:YES whileExecutingBlock:^{
+//        
+//        sleep(2);
+//        
+//    } completionBlock:^{
+//        
+//        [HUD removeFromSuperview];
+//        
+//        [HUD release];
+//        
+//        HUD = nil;
+//        
+//    }];
+
 }
 
 #pragma mark - textField delegate
+
+/**
+ textField 的代理方法
+
+ @param textField 正在操作的文本框
+ @param range 正在输入的字符添加的范围，location一般已输入的字符最后一个位置，length一直为0
+ @param string 正在输入的字符
+ @return 是否可以继续输入，如果返回FALSE则当前输入的字符不被加入到文本内容
+ */
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;
 {
-    NSString * toBeString = [textField.text stringByReplacingCharactersInRange:range withString:string]; //得到输入框的内容
+    NSString * toBeString = [textField.text stringByReplacingCharactersInRange:range withString:string]; //得到当前输入的字符输入后，输入框的全部内容内容
 //    NSLog(@"rang: from %ld  to %ld ",range.location, range.length);
     
+    //ip地址输入框
     if (self.ipTextField == textField)  //判断是否时我们想要限定的那个输入框
      {
         if ([toBeString length] > 15) { //如果输入框内容大于14则弹出警告
             NSLog(@"ipTextField 超过最大数");
+            [self showMessage:@"IP地址超过限制！"];
              return NO;
         }
     }
     
+    //房间号输入框
     if (self.roomNumTextField == textField) {
         if ([toBeString length] == 4) {
             self.ipTextField.text = [self buildIPStringWithString:toBeString];
@@ -209,6 +245,7 @@
         
         if ([toBeString length] > 4) { //如果输入框内容大于3则弹出警告
             NSLog(@"roomNumTextField 超过最大数");
+            [self showMessage:@"房间号超过限制！"];
             return NO;
         }
     }
