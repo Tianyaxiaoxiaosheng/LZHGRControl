642689e5720eeb3ee4d0202991092bd412ac08a5
Jony <jony@JonydeMacBook-Pro.local>
2017-04-23 23:30:02 +0800

    custom tabbar

--- LZHGRControl/LZMain/MainControllers/MainViewController.m
+++ LZHGRControl/LZMain/MainControllers/MainViewController.m
@@ -8,6 +8,10 @@
 
 #import "MainViewController.h"
 
+@interface MainViewController ()<WDYTabbarDelegate>
+
+@end
+
 @implementation MainViewController
 
 - (void)viewDidLoad {
@@ -15,38 +19,30 @@
     
     //设置主界面背景
     self.view.backgroundColor = MAIN_VIEW_BACKGROUND;
+    self.tabBar.backgroundColor = [UIColor colorWithRed:192/255 green:192/255 blue:192/255 alpha:0.0];
     
-    //更改默认高度
+    //替换系统Tarbar
+    CGRect tabbarFrame = CGRectMake(self.tabBar.bounds.origin.x
+                               , self.view.bounds.size.height-90
+                               , self.tabBar.bounds.size.width
+                               , 90);
+    WDYTabbar *wDYTabbar = [[WDYTabbar alloc] initWithFrame:tabbarFrame];
     
+    [wDYTabbar addTabbarBtnWithNormalImg:@"lights0" selImg:@"lights1"];
+    [wDYTabbar addTabbarBtnWithNormalImg:@"aircon0" selImg:@"aircon1"];
+    [wDYTabbar addTabbarBtnWithNormalImg:@"service0" selImg:@"service1"];
+    [wDYTabbar addTabbarBtnWithNormalImg:@"setting0" selImg:@"setting1"];
     
-    //创建控制器
-    EquipmentInfoManage *sharedEquipmentInfoTool = [EquipmentInfoManage sharedEquipmentInfoTool];
-    //灯光控制器
-    EquipmentInfo *lightInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"灯光"];
-    LightsViewController *lightsViewController = [[LightsViewController alloc] init];
-    lightsViewController.tabBarItem.title = lightInfo.title;
-    lightsViewController.tabBarItem.image = [UIImage imageNamed:@"灯光01"];
+    //设置代理
+    wDYTabbar.delegate = self;
     
-    //空调控制器
-    EquipmentInfo *airconInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"空调"];
-    AirconViewController *airconViewController = [[AirconViewController alloc] init];
-    airconViewController.tabBarItem.title = airconInfo.title;
-    airconViewController.tabBarItem.image = [UIImage imageNamed:@"空调01"];
-    
-    //服务控制器
-    EquipmentInfo *serverInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"服务"];
-    ServerViewController *serverViewController = [[ServerViewController alloc] init];
-    serverViewController.tabBarItem.title = serverInfo.title;
-    serverViewController.tabBarItem.image = [UIImage imageNamed:@"服务01"];
-    
-    //设置控制器
-    EquipmentInfo *setInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"设置"];
-    SetViewController *setViewController = [[SetViewController alloc] init];
-    setViewController.tabBarItem.title = setInfo.title;
-    setViewController.tabBarItem.image = [UIImage imageNamed:@"设置01"];
+    //把自定义的tabbar添加到 系统的tabbar上
+//    self.tabBar.frame.size.height = self.tabBar.frame.size.height+1100;
+    [self.view addSubview:wDYTabbar];
     
     //添加控制器
-    self.viewControllers = @[lightsViewController, airconViewController, serverViewController, setViewController];
+    [self addControllerForTabBarController];
+    
   
     //设置分栏
 //    UITabBar *tabBar = self.tabBar;
@@ -57,6 +53,45 @@
 //    tabBar.backgroundColor = [UIColor orangeColor];
     
 
+}
+
+//添加控制器
+- (void)addControllerForTabBarController{
+    //创建控制器
+//    EquipmentInfoManage *sharedEquipmentInfoTool = [EquipmentInfoManage sharedEquipmentInfoTool];
+    //灯光控制器
+//    EquipmentInfo *lightInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"灯光"];
+    LightsViewController *lightsViewController = [[LightsViewController alloc] init];
+    //    lightsViewController.tabBarItem.title = lightInfo.title;
+    //    lightsViewController.tabBarItem.image = [UIImage imageNamed:@"灯光01"];
+    
+    //空调控制器
+//    EquipmentInfo *airconInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"空调"];
+    AirconViewController *airconViewController = [[AirconViewController alloc] init];
+    //    airconViewController.tabBarItem.title = airconInfo.title;
+    //    airconViewController.tabBarItem.image = [UIImage imageNamed:@"空调01"];
+    
+    //服务控制器
+//    EquipmentInfo *serverInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"服务"];
+    ServerViewController *serverViewController = [[ServerViewController alloc] init];
+    //    serverViewController.tabBarItem.title = serverInfo.title;
+    //    serverViewController.tabBarItem.image = [UIImage imageNamed:@"服务01"];
+    
+    //设置控制器
+//    EquipmentInfo *setInfo = [sharedEquipmentInfoTool findEquipmentInfoWithName:@"设置"];
+    SetViewController *setViewController = [[SetViewController alloc] init];
+    //    setViewController.tabBarItem.title = setInfo.title;
+    //    setViewController.tabBarItem.image = [UIImage imageNamed:@"设置01"];
+    
+    //添加控制器
+    self.viewControllers = @[lightsViewController, airconViewController, serverViewController, setViewController];
+}
+
+#pragma mark 自定义Tabbar的代理
+-(void)tabbar:(WDYTabbar *)tabbar didSelectedFrom:(NSInteger)from to:(NSInteger)to{
+    //切换tabbar控制器的子控件器
+    self.selectedIndex = to;
+    
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ LZHGRControl/LZMain/MainView/WDYTabbar.m
@@ -0,0 +1,138 @@
+//
+//  WDYTabbar.m
+//  LZHGRControl
+//
+//  Created by Jony on 17/4/23.
+//  Copyright © 2017年 yavatop. All rights reserved.
+//
+
+#import "WDYTabbar.h"
+
+@interface WDYTabbar ()
+/**
+ * 当前选中的按钮
+ */
+@property (nonatomic, weak) UIButton *selectedBtn;
+
+@end
+
+@implementation WDYTabbar
+
+/*
+ *自定义控件
+ 1.在initWithFrame初始化的方法，添加子控件
+ 2.layoutSubviews 布局子控件frm
+ */
+
+/*
+ //调用控件的init方法【[[UIView alloc] init]】 的时候被调用  接着还会调用initWithFrame
+ //-(instancetype)init
+ 
+ 
+ //调用控件的init方法【[[UIView alloc] initWithFrame]】 的时候被调用
+ //-(instancetype)initWithFrame:(CGRect)frame
+ 
+ 
+ //调用控件的创建从xib/storybaord 的时候被调用
+ -(id)initWithCoder:(NSCoder *)aDecoder
+ */
+
+- (instancetype)initWithFrame:(CGRect)frame{
+//    NSLog(@"%lf, %lf, %lf, %lf,", frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
+//    
+    if (self = [super initWithFrame:frame]) {
+        self.backgroundColor = [UIColor grayColor];
+        
+    }
+    return  self;
+}
+
+#pragma mark 初始化按钮
+-(void)addTabbarBtnWithNormalImg:(NSString *)normalImg selImg:(NSString *)selImg{
+    UIButton *btn = [CZTabbarButton buttonWithType:UIButtonTypeCustom];
+    [btn setBackgroundImage:[UIImage imageNamed:normalImg] forState:UIControlStateNormal];
+    [btn setBackgroundImage:[UIImage imageNamed:selImg] forState:UIControlStateSelected];
+    
+    
+    //监听事件
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchDown];
+    //绑定tag
+#warning tag绑定要在addSubview的方法之前
+    btn.tag = self.subviews.count;
+    
+    [self addSubview:btn];
+    
+    //设置默认选中
+    if (btn.tag == 0) {
+        //        btn.selected = YES;
+        //        self.selectedBtn = btn;
+        [self btnClick:btn];
+    }
+    
+    
+}
+
+-(void)layoutSubviews{
+    [super layoutSubviews];
+    
+    NSInteger count = self.subviews.count;
+    //布局子控件
+    //按钮宽度与高度
+    CGFloat btnW = 80;
+    CGFloat btnH = self.bounds.size.height-10;
+    CGFloat initialX  = self.bounds.size.width/4 + 50;
+  
+    
+    //自定义的tabbar添加5个按钮
+    //    for (NSInteger i = 0; i < count; i++) {
+    //
+    //        //获取btn
+    //        UIButton *btn = self.subviews[i];
+    //
+    //        //设置按钮的frm
+    //        btn.frame = CGRectMake(btnW * i, 0, btnW, btnH);
+    //
+    //    }
+    
+    for (UIButton *btn in self.subviews) {
+        CGFloat btnIntX = (btnW+20) * btn.tag + initialX;
+        btn.frame = CGRectMake(btnIntX, 20.0, btnW, btnH);
+    }
+    
+}
+
+-(void)btnClick:(UIButton *)btn{
+    
+    //一点击通知代理
+    if ([self.delegate respondsToSelector:@selector(tabbar:didSelectedFrom:to:)]) {
+        [self.delegate tabbar:self didSelectedFrom:self.selectedBtn.tag to:btn.tag];
+    }
+    
+//#warning 开发过程，首先针对64位开发，苹果开发的应用上架，必需支持64位
+//    NSLog(@"%ld",btn.tag);
+//    
+    //取消之前选中
+    self.selectedBtn.selected = NO;
+    
+    //设置当前选中
+    btn.selected = YES;
+    self.selectedBtn = btn;
+    
+    
+}
+
+
+@end
+
+//实现自定义的btn
+@implementation CZTabbarButton
+
+//图片高亮时候会调用这个方法
+-(void)setHighlighted:(BOOL)highlighted{
+//    NSLog(@"%s",__func__);
+    //只要不调用父类的方法，按钮就不会有高度的效果
+    //[super setHighlighted:highlighted];
+}
+
+@end
+
